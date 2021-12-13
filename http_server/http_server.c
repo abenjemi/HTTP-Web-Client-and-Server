399d045a925d40b1114b674ae4bbfba9fc910cd0
abenjemi <abenjemi@purdue.edu>
2021-12-12 20:27:58 -0500

    everything works but jpg pictures

--- http_server/http_server.c
+++ http_server/http_server.c
@@ -28,11 +28,45 @@
 #define DBPORT 53004
 
 
-int isDirectory(const char *path) {
-   struct stat statbuf;
-   if (stat(path, &statbuf) != 0)
+int isDir(const char * resource) {
+   struct stat buff;
+   if (stat(resource, &buff) != 0)
+   {
        return 0;
-   return S_ISDIR(statbuf.st_mode);
+   }
+   return S_ISDIR(buff.st_mode);
+}
+
+void send_response(char * rootdir, char * response, int sockfd, int new_fd, FILE * fptr)
+{
+    fseek(fptr, 0, SEEK_END);
+    int size = ftell(fptr);
+
+    fseek(fptr, 0, SEEK_SET);
+
+    printf("200 OK\n");
+    char header[1024] = {'\0'};
+    sprintf(header, "HTTP/1.0 200 OK\r\nContent-Length: %d\r\n\r\n", size);
+    char initial_bytes[1024] = {'\0'};
+    int inbytes_read = fread(initial_bytes, 1, 1024 - strlen(header), fptr);
+    strcpy(response, header);
+    strcat(response, initial_bytes);
+
+    if(send(new_fd, response, 1024, MSG_NOSIGNAL) < 0) {
+        // perror("send");
+    }
+    memset(response, '\0', 1024);
+    size -= inbytes_read;
+    
+    while (size > 0){
+        size -= fread(response, 1, 1024, fptr);
+        if(send(new_fd, response, 1024, MSG_NOSIGNAL) < 0) {
+            // perror("send");
+        }
+        memset(response, '\0', 1024);
+        usleep(1000);
+    }  
+    
 }
 
 
@@ -40,11 +74,10 @@ int main()
 {
     int sockfd, new_fd;
 	struct sockaddr_in my_addr;
+    struct sockaddr_in their_addr;
     int sin_size;
-    char dst[INET_ADDRSTRLEN];
+    char client_ip[INET_ADDRSTRLEN];
     long bytes_read;
-
-    char *hello = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\nHello world!";
 
     // create socket
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -74,34 +107,45 @@ int main()
         // printf("\n+++++++ Waiting for new connection ++++++++\n\n");
 		sin_size = sizeof(my_addr);
 
-        if ((new_fd = accept(sockfd, (struct sockaddr *)&my_addr, (socklen_t*)&sin_size))<0)
+        if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, (socklen_t*)&sin_size)) < 0)
         {
             perror("accept");
-            exit(1);
+            continue;
         }
 
-        // printf("\nIM HERE\n");
+        // move ip address of client into client_ip
+        inet_ntop(AF_INET, &(their_addr.sin_addr), client_ip, INET_ADDRSTRLEN);
 
-        char response[1000];
-        char buf[30000] = {'\0'};
-        bytes_read = read(new_fd , buf, 30000);
+        char response[1024] = {'\0'};
+        char buf[1024] = {'\0'};
+        bytes_read = read(new_fd , buf, 1024);
         if (buf[0] != '\0')
         {
             // char * method, resource, protocol;
             char * method = strtok(buf, " ");
             char * resource = strtok(NULL, " ");
             char * protocol = strtok(NULL, "\r\n");
-            fprintf(stdout, "\"%s %s %s\" ", method, resource, protocol);
+            printf("%s \"%s %s %s\" ", client_ip, method, resource, protocol);
 
-            char rootdir[1024] = {0};
+            char rootdir[1024] = {'\0'};
             strcpy(rootdir, "Webpage");
             strcat(rootdir, resource);
 
-            if (strstr(method, "GET") == NULL) // if method used is not GET return 501
+
+            if ((strstr(protocol, "HTTP/1.1") == NULL) && (strstr(protocol, "HTTP/1.0") == NULL)) // protocol is not HTTP/1.1 or HTTP/1.0
+            {
+                printf("501 Not Implemented - protocol\n");
+                strcpy(response, "HTTP/1.0 501 Not Implemented\r\n\r\n<html><body><h1>501 Not Implemented</h1></body></html>");
+                if(send(new_fd, response, 1024, 0) < 0) 
+                {
+                    perror("send");
+                }
+            }
+            else if (strstr(method, "GET") == NULL) // if method used is not GET return 501
             {
                 printf("501 Not Implemented\n");
                 strcpy(response, "HTTP/1.0 501 Not Implemented\r\n\r\n<html><body><h1>501 Not Implemented</h1></body></html>");
-                if(send(new_fd, response, 1000, 0) < 0) 
+                if(send(new_fd, response, 1024, 0) < 0) 
                 {
                     perror("send");
                 }
@@ -110,7 +154,7 @@ int main()
             {
                 printf("400 Bad Request - resource does not start with '/'\n");
                 strcpy(response, "HTTP/1.0 400 Bad Request\r\n\r\n<html><body><h1>400 Bad Request</h1></body></html>");
-                if(send(new_fd, response, 1000, 0) == -1) {
+                if(send(new_fd, response, 1024, 0) < 0) {
                     perror("send");
                 }
             }
@@ -118,82 +162,82 @@ int main()
             {
                 printf("400 Bad Request - resource contains /../\n");
                 strcpy(response, "HTTP/1.0 400 Bad Request\r\n\r\n<html><body><h1>400 Bad Request</h1></body></html>");
-                if(send(new_fd, response, 1000, 0) == -1) {
+                if(send(new_fd, response, 1024, 0) < 0) {
                     perror("send");
                 }
             }
-            else if (strlen(resource) >= 3)
+            else if ((strlen(resource) >= 3) && (strcmp(&resource[strlen(resource) - 3], "/..") == 0))
             {
-                char bad_string[4] = "/..";
-                if (strcmp(&resource[strlen(resource) - 3], bad_string) == 0) // if resource ends with "/.." return 400
+                // char bad_string[4] = "/..";
+                
+                printf("400 Bad Request - resource ends with /..\n");
+                strcpy(response, "HTTP/1.0 400 Bad Request\r\n\r\n<html><body><h1>400 Bad Request</h1></body></html>");
+                if(send(new_fd, response, 1024, 0) < 0) {
+                    perror("send");
+                }
+                
+            }
+            else if (resource[strlen(resource) - 1] == '/') // if resource ends with / append index.html to it and return this file
+            {
+                // printf("\n\nLast character is /\n\n");
+                strcat(rootdir, "index.html");
+                FILE * fptr = fopen(rootdir, "r");
+
+                if (fptr != NULL)
                 {
-                    printf("400 Bad Request - resource ends with /..\n");
-                    strcpy(response, "HTTP/1.0 400 Bad Request\r\n\r\n<html><body><h1>400 Bad Request</h1></body></html>");
-                    if(send(new_fd, response, 1000, 0) == -1) {
+                    send_response(rootdir, response, sockfd, new_fd, fptr);
+                    fclose(fptr);
+                }
+                else
+                {
+                    printf("404 Not Found\n");
+                    strcpy(response, "HTTP/1.0 404 Not Found\r\n\r\n<html><body><h1>404 Not Found</h1></body></html>");
+                    if(send(new_fd, response, 1024, 0) < 0) {
                         perror("send");
                     }
                 }
             }
-            else if (resource[strlen(resource) - 1] == '/')
+            else if (isDir(rootdir))
             {
-                // printf("\n\n FIRST CHARACTER OF PATH IS    %c\n\n", resource[0]);
+                // printf("\n It is a directory \n");
+                strcat(rootdir, "/");
                 strcat(rootdir, "index.html");
+                // printf("\n\n %s", rootdir);
 
                 FILE * fptr = fopen(rootdir, "r");
 
                 if (fptr != NULL)
                 {
-                    fseek(fptr, 0, SEEK_END);
-                    int size = ftell(fptr);
-
-                    fseek(fptr, 0, SEEK_SET);
-
-                    printf("200 OK\n");
-                    char header[1000];
-                    memset(header, '\0', 1000);
-                    sprintf(header, "HTTP/1.0 200 OK\r\nContent-Length: %d\r\n\r\n", size);
-                    char tmp[1000];
-                    memset(tmp, '\0', 1000);
-                    int lenread = fread(tmp, 1, 1000 - strlen(header), fptr);
-                    strcpy(response, header);
-                    for(int i = 0; i < lenread; i++){
-                        response[i + strlen(header)] = tmp[i];
-                    }
-
-                    if(send(new_fd, response, 1000, 0) < 0) {
-                        perror("send");
-                    }
-                    memset(response, '\0', 1000);
-                    size -= lenread;
-                    
-                    while (size > 0){
-                        size -= fread(response, 1, 1000, fptr);
-                        if(send(new_fd, response, 1000, 0) == -1) {
-                            perror("send");
-                        }
-                        memset(response, '\0', 100);
-                        usleep(1000);
-                    }
-                    
+                    send_response(rootdir, response, sockfd, new_fd, fptr);
                     fclose(fptr);
                 }
                 else
                 {
-                    fprintf(stdout, " 404 Not Found\n");
+                    printf("404 Not Found, no index\n");
                     strcpy(response, "HTTP/1.0 404 Not Found\r\n\r\n<html><body><h1>404 Not Found</h1></body></html>");
-                    if(send(new_fd, response, 1000, 0) == -1) {
+                    if(send(new_fd, response, 1024, 0) < 0) {
                         perror("send");
                     }
-                    memset(response, '\0', 1000);
                 }
-
             }
-            // else if ()
+            else
+            {
+                FILE * fptr = fopen(rootdir, "r");
 
-            // printf("%s\n",buf);
-            // ssize_t bytes = write(new_fd , hello , strlen(hello));
-            // printf("------------------Hello message sent-------------------");
-
+                if (fptr != NULL)
+                {
+                    send_response(rootdir, response, sockfd, new_fd, fptr);
+                    fclose(fptr);
+                }
+                else
+                {
+                    printf("404 Not Found\n");
+                    strcpy(response, "HTTP/1.0 404 Not Found\r\n\r\n<html><body><h1>404 Not Found</h1></body></html>");
+                    if(send(new_fd, response, 1024, 0) < 0) {
+                        perror("send");
+                    }
+                }
+            }
         }
         
         close(new_fd);
